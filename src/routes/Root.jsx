e22e4666cf733ab4c14c442cd8963dc8d67c32e9
uchario <uchario@yahoo.com>
2023-09-15 04:46:37 +0100

    Change 'header' import to 'Header'

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,6 +1,6 @@
 import Footer from "../components/Footer";
 import Main from "../components/Main";
-import Header from "../components/header";
+import Header from "../components/Header";
 
 const Root = () => {
     return(
