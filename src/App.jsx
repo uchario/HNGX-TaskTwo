3b7eedae61c4aa2c1248213564bd94ed240099c8
uchario <uchario@yahoo.com>
2023-09-15 04:24:57 +0100

    Update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import Root from './routes/Root';
+import MovieDetail from './routes/MovieDetail';
+import Error from './routes/Error';
+
+import { loader as moviesLoader } from './loader/movie-loader';
+import { loader as movieLoader } from './loader/movie-detail-loader';
 
 const router = createBrowserRouter([
   {
@@ -7,7 +13,16 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Root/>
+        element: <Root/>,
+        id: 'movies',
+        loader: moviesLoader,
+        errorElement: <Error/>
+      },
+      {
+        path: 'movies/:id',
+        element: <MovieDetail/>,
+        id: 'movie-detail',
+        loader: movieLoader
       }
     ]
 
